ebc1cc170209d84e0f0dcf6bee231816e506a1ee
Parameshwari2004 <116253214+Parameshwari2004@users.noreply.github.com>
2023-06-23 10:38:11 +0530

    day17 update

---
+++ src/test/java/com/fssa/paramu/day17/solved/HelloTest.java
@@ -0,0 +1,16 @@
+package com.fssa.paramu.day17.solved;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+	/**
+	 * First test case
+	 */
+	@Test
+	public void helloTest() {
+		
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
+} 
