ed64f81f8fbe9a1a6e02a689a20e5a12a9160660
Parameshwari2004 <116253214+Parameshwari2004@users.noreply.github.com>
2023-06-26 20:09:15 +0530

    day18update

---
+++ src/main/java/com/fssa/learnJava/fop/day18/ValidationUtil1.java
@@ -0,0 +1,12 @@
+package com.fssa.learnJava.fop.day18;
+
+public class ValidationUtil1 {
+	public static boolean validateAge(int age) throws IllegalArgumentException {
+		if (age <= 0) {// Check if the age is a posotive value
+			throw new IllegalArgumentException("Invalid age");
+
+		} else {
+			return true;
+		}
+	}
+}
